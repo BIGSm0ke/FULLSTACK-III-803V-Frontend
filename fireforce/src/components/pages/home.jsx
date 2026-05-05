63b9e76118ea1ba5b69f3cceb38cdb3f9eaa3287
GatitoSabor <168875168+GatitoSabor@users.noreply.github.com>
2026-05-05 10:34:48 -0400

    V1.1 Creación page reportes

--- fireforce/src/components/pages/home.jsx
+++ fireforce/src/components/pages/home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useNavigate } from 'react-router-dom';
+import L from 'leaflet';
+import { useReports } from '../../context/ReportContext';
 import 'leaflet/dist/leaflet.css';
 import '../../styles/home.css';
 
+const deleteDefaultIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
+
 const Home = () => {
     const navigate = useNavigate();
+    const { reports } = useReports();
 
     return (
         <div className="home-container">
@@ -25,7 +37,7 @@ const Home = () => {
                 <div className="home-block home-map-block">
                     <MapContainer
                         className="home-map"
-                        center={[-12.0464, -77.0428]}
+                        center={[-33.4489, -70.6693]}
                         zoom={13}
                         scrollWheelZoom={false}
                         dragging={true}
@@ -33,6 +45,17 @@ const Home = () => {
                         attributionControl={true}
                     >
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        {reports.map(report => (
+                            <Marker key={report.id} position={[report.lat, report.lng]} icon={deleteDefaultIcon}>
+                                <Popup>
+                                    <strong>Incendio {report.fireType}</strong><br />
+                                    Gravedad: {report.severity}<br />
+                                    Se observa: {report.visible}<br />
+                                    Dirección: {report.address}<br />
+                                    <em>Reportado por: {report.name}</em>
+                                </Popup>
+                            </Marker>
+                        ))}
                     </MapContainer>
                     <button className="try-map-btn" onClick={() => navigate('/monitoreo')}>
                         ¿Quieres reportar un incendio?
